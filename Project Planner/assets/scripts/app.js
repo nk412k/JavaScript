3ae12e952fedee00796fd24662a9841f1d554c50
Nokhalal MAhato <Nokhalal412000@gmail.com>
2021-10-26 15:02:27 +0530

    Nk

--- Project Planner/assets/scripts/app.js
+++ Project Planner/assets/scripts/app.js
@@ -1,16 +1,60 @@
-class Tooltip {}
+class Tooltip {
+    constructor(closeNotifierFunc){
+        this.closeNotifier=closeNotifierFunc;
+    }
+    clooseTooltip = () => {
+        this.detach();
+        this.closeNotifier();
+    }
+    detach(){
+        this.element.remove();
+    }
+    attach(){
+        const newElement=document.createElement('div')
+        newElement.className='card';
+        newElement.textContent='Dummy';
+        newElement.addEventListener('click',this.clooseTooltip)
+        this.element=newElement;
+        document.body.append(newElement);
+    }
+}
 
 class ProjectItem {
-    constructor(id,updateProjectListsfunction){
+    hasActiveTooltip=false;
+    constructor(id,updateProjectListsfunction,type){
         this.id=id;
         this.updateProjectListsHandler=updateProjectListsfunction;
-        this.connectSwithButton();
+        this.connectSwithButton(type);
+        this.connectMoreInfo();
+    }
+    showMoreInfoHandler(){
+        if(this.hasActiveTooltip){
+            return;
+        }
+        const tooltip=new Tooltip(() =>{
+            this.hasActiveTooltip=false;
+        });
+        tooltip.attach();
+        this.hasActiveTooltip=true;
     }
 
-    connectSwithButton(){
+    connectMoreInfo(){
         const projectItemElemnet=document.getElementById(this.id);
-        const switchBtn=projectItemElemnet.querySelector('button:last-of-type')
-        switchBtn.addEventListener('click',this.updateProjectListsHandler);
+        const moreinfoBtn=projectItemElemnet.querySelector('button:first-of-type');
+        moreinfoBtn.addEventListener('click',this.showMoreInfoHandler);
+    }
+
+    connectSwithButton(type){
+        const projectItemElemnet=document.getElementById(this.id);
+        const switchBtn=projectItemElemnet.querySelector('button:last-of-type');
+        switchBtn.textContent= type==='active'?'Finish':'Activate';
+        switchBtn.addEventListener('click',this.updateProjectListsHandler.bind(null,this.id));
+        switchBtn.removeEventListener('click',this.updateProjectListsHandler.bind(null,this.id));
+    }
+
+    update(updateProjectListfunc,type){
+        this.updateProjectListsHandler=updateProjectListfunc;
+        this.connectSwithButton(type);
     }
     
 }
@@ -18,9 +62,10 @@ class ProjectItem {
 class ProjectLists{
     projects=[];
     constructor(type){
+        this.type=type;
         const prjItems=document.querySelectorAll(`#${type}-projects li`);
         for(const prjItem of prjItems){
-            this.projects.push(new ProjectItem(prjItem.id,this.switchProject.bind(this)));
+            this.projects.push(new ProjectItem(prjItem.id,this.switchProject.bind(this),this.type));
         }
         console.log(this.projects);
     }
@@ -28,12 +73,23 @@ class ProjectLists{
         this.switchHandler=switchHandlerFunction;
 }
 
-    addProject(){
-        console.log(this);
+    addProject(project){
+        this.projects.push(project);
+        DOMHelper.moveElement(project.id,`#${this.type}-projects ul`);
+        project.update(this.switchProject.bind(this),this.type)  
+
     }
     switchProject(projectId){
         this.switchHandler(this.projects.find(p => p.id === projectId));
         this.projects=this.projects.filter(p => p!== projectId);
+    }
+}
+
+class DOMHelper{
+    static moveElement(projectId,newDestination){
+        const element=document.getElementById(projectId);
+        const destination=document.querySelector(newDestination);
+        destination.append(element);
     }
 }
 
